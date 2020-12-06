cc0fe042c22df15d093b1ad3cd25853983004b89
Brandon <brandon.monier@gmail.com>
2020-12-06 15:08:21 -0500

    Solved Day 6, Part 1 and Part 2

--- src/R/06.R
+++ src/R/06.R
@@ -8,12 +8,47 @@ input <- scan(
     blank.lines.skip = FALSE
 )
 
+yesCounter <- function(x) {
+    strsplit(x, split = "") %>% 
+        unlist() %>% 
+        unique() %>% 
+        sort() %>% 
+        length()
+}
+
 questions <- split(
     x = input,
     f = with(rle(input == ""), rep(seq_along(values), lengths))
-) %>% .[. != ""]
+) %>% 
+    .[. != ""] %>% 
+    lapply(., yesCounter)
 
+answerP1 <- questions %>% unlist() %>% sum()
 
 
 # Part 2 ----
+yesTabler <- function(x) {
+    strsplit(x, split = "") %>% 
+        unlist() %>% 
+        table()
+}
+
+questions <- split(
+    x = input,
+    f = with(rle(input == ""), rep(seq_along(values), lengths))
+) %>% 
+    .[. != ""]
+
+counts <- lapply(questions, length)
+
+questions <- questions %>% lapply(yesTabler)
+
+answerP2 <- lapply(seq_along(questions), function(i) {
+    questions[[i]] %in% counts[[i]] %>% 
+        .[.] %>% 
+        length()
+}) %>% 
+    unlist() %>% 
+    sum()
+
 
