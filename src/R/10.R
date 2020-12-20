063077c84a2601c1d32506233df6069330d45ee7
Brandon <brandon.monier@gmail.com>
2020-12-20 00:39:16 -0500

    Add data for Day 10

---
+++ src/R/10.R
@@ -0,0 +1,12 @@
+# Part 1 ----
+input <- scan(
+    file = "input/test.txt",
+    what = numeric(),
+    sep = "\n",
+    blank.lines.skip = FALSE
+)
+joltAdapt <- max(input) + 3
+
+
+
+# Part 2 ----
