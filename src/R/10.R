09125817991fd822dd348a4740a1da256a8cfe41
Brandon <brandon.monier@gmail.com>
2020-12-25 20:06:35 -0500

    Initial commit

--- src/R/10.R
+++ src/R/10.R
@@ -4,8 +4,19 @@ input <- scan(
     what = numeric(),
     sep = "\n",
     blank.lines.skip = FALSE
-)
-joltAdapt <- max(input) + 3
+) %>%
+    data.frame(
+        x = .,
+        visit = FALSE,
+        vGen = 0,
+        d1 = 0,
+        d2 = 0,
+        d3 = 0
+    )
+joltAdapt <- max(input$x) + 3
+
+tmp <- input[input$x <= 3 & !input$visit, ]
+tmp <- tmp[min(tmp$x), ]
 
 
 
